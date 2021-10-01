e97c6da078dd24fa1d0f2d31212bba81cde63d2b
shOuru4997 <souravkumar4997@gmail.com>
2021-10-01 09:54:03 +0530

    Updates

---
+++ precache-manifest.0ebd2b6af99f3b0d19441100163af445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab842ae06590d06ff304d775674a780",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "57d78ebb767c776616cd",
+    "url": "/React-Calculator/static/css/main.3bac6953.chunk.css"
+  },
+  {
+    "revision": "b011ce21a6da794d9886",
+    "url": "/React-Calculator/static/js/2.add7bac3.chunk.js"
+  },
+  {
+    "revision": "57d78ebb767c776616cd",
+    "url": "/React-Calculator/static/js/main.a78b2ad7.chunk.js"
+  },
+  {
+    "revision": "19a8db08324e9b1cabe9",
+    "url": "/React-Calculator/static/js/runtime~main.cb778861.js"
+  }
+]);
